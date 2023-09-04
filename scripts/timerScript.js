e2d336ee9b132f442ba959284611c273a7e20f47
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-04 19:39:29 -0300

    feat: Definition of variables and function to start time

--- scripts/timerScript.js
+++ scripts/timerScript.js
@@ -3,3 +3,16 @@ const getSeconds = (seconds) => {
   return date.toLocaleTimeString("pt-Br", { hour12: false, timeZone: "UTC" });
 };
 
+const clock = document.querySelector(".time-wrapper");
+const start = document.querySelector(".start");
+const pause = document.querySelector(".pause");
+const restart = document.querySelector(".restart");
+let second = 0;
+let timer;
+
+const startClock = () => {
+  timer = setInterval(() => {
+    second++, (clock.innerHTML = getSeconds(second));
+  }, 1000);
+};
+
